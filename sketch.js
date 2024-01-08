89fed157338437ea6d2f36820984c9668206e58f
JimmyCauja <jimmycauja@gmail.com>
2024-01-08 16:49:03 -0500

    se convirtieron a imagenes png

--- sketch.js
+++ sketch.js
@@ -1,5 +1,20 @@
+const celdas = [];
+const RETICULA = 5;
+
+const azulejos = 5;
+const NA = 17;
+const reglas = [
+  {
+    //tile 0
+    UP; 0,
+    RIGHT; 0,
+    DOWN; 0,
+    LEFT; 0,
+  },
+];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeigth);
 }
 
 function draw() {
